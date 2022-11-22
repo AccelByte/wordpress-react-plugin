4796193d082b4aae3a83d1b3f6044c32c835bf58
wahyu arzani <wahyu.arzani@accelbyte.net>
2022-11-22 11:39:22 +0700

    fix(profile): SA-558 change avatar url from bridge

--- widget/src/api/user/profile.ts
+++ widget/src/api/user/profile.ts
@@ -8,7 +8,7 @@ import { apiUrl } from "src/utils/env";
 import { combineURLPaths } from "src/utils/urlHelper";
 import { Network } from "../network";
 import { guardNetworkCall } from "../networkCallTypeguard";
-import { UserProfile, UserProfileDecodeError } from "./models/profile";
+import { UserProfile, UserProfileDecodeError, UserProfileAvatar } from "./models/profile"; // SA-558 The avatar in paydaythegame.com is not the same as in my account
 
 export async function fetchUserProfile(network: Network, { namespace }: { namespace: string }) {
   const url = combineURLPaths(apiUrl, `basic/v1/public/namespaces/${namespace}/users/me/profiles`)
@@ -19,3 +19,15 @@ export async function fetchUserProfile(network: Network, { namespace }: { namesp
     (error) => error
   );
 }
+
+// SA-558 The avatar in paydaythegame.com is not the same as in my account: start
+export async function fetchUserProfileAvatar(network: Network, { namespace }: { namespace: string }) {
+  const url = combineURLPaths(apiUrl, `bridge/v1/public/namespaces/${namespace}/users/me/profiles`)
+  return guardNetworkCall(
+    () => network.get(url),
+    UserProfileAvatar,
+    UserProfileDecodeError,
+    (error) => error
+  );
+}
+// SA-558 The avatar in paydaythegame.com is not the same as in my account: end
